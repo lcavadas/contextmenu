9af1c951a1ca5f6aa676d2a6ef922e4b5915c723
Luís Serralheiro <encavadas@gmail.com>
2015-11-26 17:47:59 +0000

    Adding show and hide callbacks

--- contextmenu.js
+++ contextmenu.js
@@ -1,98 +1,108 @@
-/* globals jQuery:true */
-/*!
- * jQuery Plugin for Context Menus
- *
- * @author Luis Serralheiro 2015
- */
-(function ($) {
-  $.contextmenuGlobalHandler = function () {
-    $('.cm-wrapper').remove();
-  };
+/*global jQuery:true*/
+(function() {
+    var ev = new jQuery.Event('remove'),
+        orig = jQuery.fn.remove;
+    jQuery.fn.remove = function() {
+        jQuery(this).trigger(ev);
+        return orig.apply(this, arguments);
+    };
+})();
 
-  $.fn.contextmenu = function (options) {
-    var _$this = $(this);
+jQuery.contextmenuGlobalHandler = function () {
+    jQuery('.cm-wrapper').remove();
+};
+
+jQuery.fn.contextmenu = function (options) {
+    var _$this = jQuery(this);
+
     var _$menuContainer;
-    var settings = $.extend({
-      offsetX: 0,
-      offsetY: 0,
-      width: 80,
-      boundsEl: document.body,
-      options: [
-        {
-          display: '<span>option 1</span>',
-          value: 'op 1'
+    var settings = jQuery.extend({
+        offsetX: 0,
+        offsetY: 0,
+        width: 80,
+        boundsEl: this,
+        options: [
+            {
+                display: '<span>option 1</span>',
+                value: 'op 1'
+            },
+            {
+                display: '<span>option 2</span>',
+                value: 'op 2'
+            }
+        ],
+        cb: function (value) {
+            window.console.log(value);
         },
-        {
-          display: '<span>option 2</span>',
-          value: 'op 2'
+        show: function () {
+        },
+        hide: function () {
         }
-      ],
-      cb: function (value) {
-        window.console.log(value);
-      }
     }, options);
 
-    var _show = function (e) {
-      e.preventDefault();
-      e.stopPropagation();
-
-      if (settings.options.length === 0) {
-        return;
-      }
-
-      var parentOffset = _$this.offset();
-      $('.cm-wrapper').remove();
-      _$menuContainer = $('<div class="cm-wrapper" style="visibility: hidden;width:' + settings.width + 'px;margin-left:' + (e.pageX - parentOffset.left + settings.offsetX) + 'px;margin-top:' + (e.pageY - parentOffset.top + settings.offsetY) + 'px"><ol class="cm-menu"></ol></div>');
-      var _$menu = _$menuContainer.find('ol');
-      _$this.before(_$menuContainer);
-
-      window.setTimeout(function () {
-        var verticalEdge = _$menuContainer.offset().top + _$menuContainer.outerHeight();
-        var horizontalEdge = _$menuContainer.offset().left + _$menuContainer.outerWidth();
-        var verticalMax = $(settings.boundsEl).offset().top + $(settings.boundsEl).height();
-        var horizontalMax = $(settings.boundsEl).offset().left + $(settings.boundsEl).width();
-
-        if (verticalEdge > verticalMax) {
-          var vpos = _$menuContainer.css('margin-top');
-          vpos = parseInt(vpos.substring(0, vpos.length - 2));
-          _$menuContainer.css('margin-top', (vpos - _$menuContainer.outerHeight()) + 'px');
+    var _hide = function () {
+        if (_$menuContainer) {
+            _$menuContainer.remove();
         }
-
-        if (horizontalEdge > horizontalMax) {
-          var hpos = _$menuContainer.css('margin-left');
-          hpos = parseInt(hpos.substring(0, hpos.length - 2));
-          _$menuContainer.css('margin-left', (hpos - _$menuContainer.outerWidth()) + 'px');
-        }
-
-        _$menuContainer.css('visibility', 'visible');
-      }, 1);
-
-      settings.options.forEach(function (op) {
-        var _$option = $('<li value="' + op.value + '">' + op.display + '</li>');
-        _$menu.append(_$option);
-        _$option.click(function () {
-          settings.cb(op.value);
-          _hide();
-        });
-      });
     };
 
-    var _hide = function () {
-      if (_$menuContainer) {
-        _$menuContainer.remove();
-      }
+    var _show = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        if (settings.options.length === 0) {
+            return;
+        }
+
+        var parentOffset = _$this.offset();
+        jQuery('.cm-wrapper').remove();
+        _$menuContainer = jQuery('<div class="cm-wrapper" style="visibility: hidden;width:' + settings.width + 'px;margin-left:' + (e.pageX - parentOffset.left + settings.offsetX) + 'px;margin-top:' + (e.pageY - parentOffset.top + settings.offsetY) + 'px"><ol class="cm-menu"></ol></div>');
+        _$menuContainer.on("remove", settings.hide);
+        var _$menu = _$menuContainer.find('ol');
+        _$this.before(_$menuContainer);
+
+        window.setTimeout(function () {
+            var verticalEdge = _$menuContainer.offset().top + _$menuContainer.outerHeight();
+            var horizontalEdge = _$menuContainer.offset().left + _$menuContainer.outerWidth();
+            var verticalMax = jQuery(settings.boundsEl).offset().top + jQuery(settings.boundsEl).height();
+            var horizontalMax = jQuery(settings.boundsEl).offset().left + jQuery(settings.boundsEl).width();
+
+            if (verticalEdge > verticalMax) {
+                var vpos = _$menuContainer.css('margin-top');
+                vpos = parseInt(vpos.substring(0, vpos.length - 2));
+                _$menuContainer.css('margin-top', (vpos - _$menuContainer.outerHeight()) + 'px');
+            }
+
+            if (horizontalEdge > horizontalMax) {
+                var hpos = _$menuContainer.css('margin-left');
+                hpos = parseInt(hpos.substring(0, hpos.length - 2));
+                _$menuContainer.css('margin-left', (hpos - _$menuContainer.outerWidth()) + 'px');
+            }
+
+            _$menuContainer.css('visibility', 'visible');
+        }, 1);
+
+        settings.options.forEach(function (op) {
+            var _$option = jQuery('<li value="' + op.value + '">' + op.display + '</li>');
+            _$menu.append(_$option);
+            _$option.click(function () {
+                settings.cb(op.value);
+                _hide();
+            });
+        });
+
+        settings.show();
     };
 
     var _init = function () {
-      $(document.body).unbind("click", $.contextmenuGlobalHandler).bind("click", $.contextmenuGlobalHandler);
-      _$this.bind('contextmenu', function (e) {
-        _show(e);
-      });
+        jQuery(document.body).unbind("click", jQuery.contextmenuGlobalHandler).bind("click", jQuery.contextmenuGlobalHandler);
+        _$this.bind('contextmenu', function (e) {
+            _show(e);
+        });
     };
     _init();
 
     return {
-      hide: _hide
+        hide: _hide
     };
-  };
-}(jQuery));
+};
